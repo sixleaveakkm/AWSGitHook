ed0241016713f05a35463443b15b2c230b28ab88
Sixleaveakkm <sixleaveakkm@gmail.com>
2019-03-26 23:42:59 +0900

    strip double quotes

--- src/script/outer_build.sh
+++ src/script/outer_build.sh
@@ -7,9 +7,9 @@ git clone $cloneURL "repo"
 git config --global user.name "DevBot"
 git config --global user.email "new_auth_dev_bot@worksap.co.jp"
 
-trigger_event=`cat git_info.json | jq .event`
-source_branch=`cat git_info.json | jq .source`
-destination_branch=`cat git_info.json | jq .destination`
+trigger_event=`cat git_info.json | jq .event | sed -ne "s/\"\(.*\)\"/\1/p"`
+source_branch=`cat git_info.json | jq .source | sed -ne "s/\"\(.*\)\"/\1/p"`
+destination_branch=`cat git_info.json | jq .destination | sed -ne "s/\"\(.*\)\"/\1/p"`
 cd repo
 git checkout $destination_branch
 if [[ $trigger_event =~ pullrequest.* ]];then
@@ -18,7 +18,7 @@ fi
 
 gitConnector build_start
 
-bash -x `cat ../git_info.json | jq .ExecutePath`
+bash -x `cat ../git_info.json | jq .ExecutePath | sed -ne "s/\"\(.*\)\"/\1/p"`
 code=$?
 if (( $? == 0 ));then
     gitConnector build_succ
