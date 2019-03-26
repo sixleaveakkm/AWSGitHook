745e99339fb2a517a6bf9cb4306745654a45080b
sixleaveakkm <sixleaveakkm@gmail.com>
2019-03-26 21:47:45 +0900

    - bug fix

--- src/script/outer_build.sh
+++ src/script/outer_build.sh
@@ -9,6 +9,7 @@ git config --global user.email "new_auth_dev_bot@worksap.co.jp"
 trigger_event=`cat git_info.json | jq .event`
 source_branch=`cat git_info.json | jq .source`
 destination_branch=`cat git_info.json | jq .destination`
+cd repo
 git checkout $destination_branch
 if [[ $trigger_event =~ pullrequest.* ]];then
     git merge $source_branch
@@ -18,12 +19,12 @@ PATH=$PATH:`pwd`
 
 ./gitConnector build_start
 
-cd repo
-bash -x `./gitConnector executePath`
+
+bash -x `cat ../git_info.json | jq .ExecutePath`
 code=$?
 if (( $? == 0 ));then
-    ./gitConnector build_succ
+    gitConnector build_succ
 else
-    ./gitConnector build_fail
+    gitConnector build_fail
 fi
 exit $code
